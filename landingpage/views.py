63c7fd78d903792e9b32a34774d348e82810830b
MandiriTraining <111072208+MandiriTraining@users.noreply.github.com>
2022-08-15 09:35:32 +0700

    initiate home landing page

--- landingpage/views.py
+++ landingpage/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Belajar Django Luuur")
+    # return HttpResponse("Belajar Django Luuur")
+    return render(request,'landingpage/index.html')
